e8f9aa4a6e43f8bfb537eac0de64c56944ea3c45
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-04-16 02:17:19 +0530

    added colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = const Color(0xff13aaff);
+Color whiteColor = Colors.white;
+Color grey1 = const Color(0xfff2f2f2);
+Color grey2 = const Color(0xff797979);
+Color grey3 = const Color(0xffd9d9d9);
