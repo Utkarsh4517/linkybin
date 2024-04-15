1eb19447cdec4c284010307e395c7374e80a1539
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-04-16 02:59:26 +0530

    create linkybin model

---
+++ lib/src/home/data/model/linkybin.model.dart
@@ -0,0 +1,10 @@
+class LinkyBinModel {
+  final String url;
+  final String category;
+  final String shortUrl;
+  LinkyBinModel({
+    required this.url,
+    this.category = '',
+    this.shortUrl = ''
+  });
+}
