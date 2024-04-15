93fe2f564ad2632dbd42745f8eca227650e753e4
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-04-16 03:44:28 +0530

    created json serialization for LinkybinModel

--- lib/src/home/data/model/linkybin.model.dart
+++ lib/src/home/data/model/linkybin.model.dart
@@ -1,10 +1,28 @@
+import 'dart:convert';
+
 class LinkyBinModel {
   final String url;
   final String category;
   final String shortUrl;
-  LinkyBinModel({
-    required this.url,
-    this.category = '',
-    this.shortUrl = ''
-  });
+  LinkyBinModel({required this.url, this.category = '', this.shortUrl = ''});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'url': url,
+      'category': category,
+      'shortUrl': shortUrl,
+    };
+  }
+
+  factory LinkyBinModel.fromMap(Map<String, dynamic> map) {
+    return LinkyBinModel(
+      url: map['url'] ?? '',
+      category: map['category'] ?? '',
+      shortUrl: map['shortUrl'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory LinkyBinModel.fromJson(String source) => LinkyBinModel.fromMap(json.decode(source));
 }
